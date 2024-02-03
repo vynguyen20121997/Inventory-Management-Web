7ef85b5cead82928023bf736df3600b0fdf7f9af
unknown <duynhannguyenn@gmail.com>
2024-02-03 18:10:35 +0700

    feat(IMS-3): delete unuse code

--- SMW/src/feature/login/LoginPage.jsx
+++ SMW/src/feature/login/LoginPage.jsx
@@ -1,12 +1,7 @@
-import NumberInput from '../../components/NumberInput/NumberInput';
-import TextInput from '../../components/textInput/TextInput';
-
 const LoginPage = () => {
   return (
     <>
       <div className=" ">LoginPage</div>
-      <TextInput />
-      <NumberInput />
     </>
   );
 };
