8e129243c093cb52e7cc4a76485513a886092713
unknown <duynhannguyenn@gmail.com>
2024-02-03 17:48:43 +0700

    feat(IMS-3): Build textInput

---
+++ SMW/src/components/textInput/TextInput.jsx
@@ -0,0 +1,45 @@
+import { useState } from 'react';
+import PropTypes from 'prop-types';
+
+const TextInput = ({
+  width,
+  height,
+  backGround,
+  TextLabel,
+  placeHolder,
+  onTextChange,
+}) => {
+  const [text, setText] = useState('');
+  const customCssInput = `${width} ${height} ${backGround}  `;
+
+  const handleInputChange = (event) => {
+    const inputValue = event.target.value;
+    setText(inputValue);
+    onTextChange(inputValue);
+  };
+  return (
+    <>
+      {TextLabel ? <label htmlFor={TextLabel}> {TextLabel} </label> : ''}
+
+      <input
+        id={TextLabel}
+        placeholder={placeHolder}
+        type="text"
+        className={` ${customCssInput}  outline-none transition-all hover:outline-colorMain focus:outline-colorMain rounded `}
+        value={text}
+        onChange={handleInputChange}
+      />
+    </>
+  );
+};
+
+TextInput.propTypes = {
+  placeHolder: PropTypes.string,
+  TextLabel: PropTypes.string,
+  width: PropTypes.string,
+  height: PropTypes.string,
+  backGround: PropTypes.string,
+  onTextChange: PropTypes.func,
+};
+
+export default TextInput;
--- SMW/src/feature/login/LoginPage.jsx
+++ SMW/src/feature/login/LoginPage.jsx
@@ -1,7 +1,19 @@
+import TextInput from '../../components/textInput/TextInput';
+
 const LoginPage = () => {
+  const getText = (value) => {
+    console.log(value);
+  };
   return (
     <>
       <div className=" ">LoginPage</div>
+      <TextInput
+        width={'100px'}
+        height={'100px'}
+        placeHolder={'Nhap tai khoan'}
+        backGround={'bg-blue-500'}
+        onTextChange={getText}
+      />
     </>
   );
 };
