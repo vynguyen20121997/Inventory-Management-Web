7bd923bab0ee581084849614b980b2d9af4ff96a
vynguyen20121997 <133662722+vynguyen20121997@users.noreply.github.com>
2024-02-23 15:45:36 +0700

    delete and move unused code

--- SMW/src/tests/dataShelf.jsx
+++
@@ -1,7 +0,0 @@
-export const shelfColumns = [
-  { accessorKey: "shelfName", cell: (info) => <div> {info.getValue()} </div> },
-  {
-    accessorKey: "shelfTotalItem",
-    cell: (info) => <div> {info.getValue()} </div>,
-  },
-];
